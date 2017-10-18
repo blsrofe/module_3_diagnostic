78da18ae3fccc6c754e1e21ae843be9ed2826454
Becki Srofe <blf_23@yahoo.com>
2017-10-18 10:16:37 -0600

    Add view and some faraday info

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    conn = Faraday.new(:url => 'http://sushi.com') do |faraday|
+      faraday.adapter  Faraday.default_adapter 
+    end
   end
 end
