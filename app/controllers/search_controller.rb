5fdace080c3397477ca4f79ea1934c3106869d0f
Becki Srofe <blf_23@yahoo.com>
2017-10-18 10:32:52 -0600

    Add more Faraday info, not yet working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(:url => 'http://sushi.com') do |faraday|
-      faraday.adapter  Faraday.default_adapter 
+    conn = Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1/') do |faraday|
+      faraday.adapter  Faraday.default_adapter
     end
+    response = conn.get '/nearest.json', { fuel_type: 'LPG,ELEC', location: '80203',
+                                          limit: '10', api_key: 'ENV['API_KEY']',
+                                          format: 'JSON', radius: '6.0'
+                                          }
+                                          binding.pry
   end
 end
