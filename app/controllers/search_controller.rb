debfd2b0e5f4319cd01b563b9cd4b5752929a9c3
Becki Srofe <blf_23@yahoo.com>
2017-10-18 10:48:07 -0600

    Add new attempt at Faraday, appears to be working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,20 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1/') do |faraday|
-      faraday.adapter  Faraday.default_adapter
+    # conn = Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1/') do |faraday|
+    #   faraday.adapter  Faraday.default_adapter
+    # end
+    # response = conn.get '/nearest.json', { fuel_type: 'LPG,ELEC', location: '80203',
+    #                                       limit: '10', api_key: ENV['API_KEY'],
+    #                                       format: 'JSON', radius: '6.0'
+    #                                       }
+    response = Faraday.get 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json' do |req|
+      req.params['fuel_type'] = 'LPG,ELEC'
+      req.params['location'] = '80203'
+      req.params['limit'] = '10'
+      req.params['api_key'] = ENV['API_KEY']
+      req.params['format'] = 'JSON'
+      req.params['radius'] = '6.0'
     end
-    response = conn.get '/nearest.json', { fuel_type: 'LPG,ELEC', location: '80203',
-                                          limit: '10', api_key: 'ENV['API_KEY']',
-                                          format: 'JSON', radius: '6.0'
-                                          }
-                                          binding.pry
+                                        
   end
 end
